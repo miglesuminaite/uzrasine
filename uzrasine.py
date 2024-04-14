310d73761a54d753e2805e0b6ab22b92fcaaef6c
miglesuminaite <159789395+miglesuminaite@users.noreply.github.com>
2024-04-14 13:55:09 +0300

    Update uzrasine
    prideti html templates, sukurti 2 marsrutai POST uzklausoms

--- uzrasine.py
+++ uzrasine.py
@@ -12,5 +12,17 @@ def uzrasai():
     uzrasai = [...]  # Čia gauti užrašų duomenis
     return render_template('uzrasai.html', uzrasai=uzrasai)
 
+@app.route('/vartotojai', methods=['POST'])
+def sukurti_vartotoja():
+    duomenys = request.form  # Gauti duomenis iš POST užklausos formos
+    # Čia įvykdyti operacijas su gautais duomenimis (pvz., įrašyti į duomenų bazę)
+    return 'Vartotojas sukurtas'
+
+@app.route('/uzrasai', methods=['POST'])
+def sukurti_uzrasa():
+    duomenys = request.form  # Gauti duomenis iš POST užklausos formos
+    # Čia įvykdyti operacijas su gautais duomenimis (pvz., įrašyti į duomenų bazę)
+    return 'Užrašas sukurtas'
+
 if __name__ == '__main__':
     app.run(debug=True)
